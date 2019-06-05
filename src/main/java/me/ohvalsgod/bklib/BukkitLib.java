8ed48a3b2199901a3125f75a01ed4434befee0ec
ohvalsgod <pikayou04@gmail.com>
2019-06-04 20:07:38 -0500

    initial commit

--- src/main/java/me/ohvalsgod/bklib/BukkitLib.java
+++ src/main/java/me/ohvalsgod/bklib/BukkitLib.java
@@ -1,4 +1,25 @@
 package me.ohvalsgod.bklib;
 
-public class BukkitLib {
+import lombok.Getter;
+import me.ohvalsgod.bklib.board.sidebar.SidebarHandler;
+import me.ohvalsgod.bklib.board.sidebar.example.ExampleAdapter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BukkitLib extends JavaPlugin {
+
+    @Getter private static BukkitLib library;
+
+    @Getter private SidebarHandler sidebarHandler;
+
+    @Override
+    public void onEnable() {
+        library = this;
+
+        sidebarHandler = new SidebarHandler(library, new ExampleAdapter());
+    }
+
+    @Override
+    public void onDisable() {
+        library = null;
+    }
 }
