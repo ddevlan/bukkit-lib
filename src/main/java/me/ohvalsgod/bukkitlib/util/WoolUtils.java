9a2fbaa56ee0f87529fb782e052618053adc77f1
ohvalsgod <pikayou04@gmail.com>
2019-06-14 12:36:05 -0500

    Added color utils :D

---
+++ src/main/java/me/ohvalsgod/bukkitlib/util/WoolUtils.java
@@ -0,0 +1,27 @@
+package me.ohvalsgod.bukkitlib.util;
+
+import org.bukkit.ChatColor;
+
+public class WoolUtils {
+
+    private static byte[] data;
+
+    static {
+        data = new byte[] {
+                15, 11, 13, 9, 14, 10, 1, 8, 7, 3, 5, 9, 14, 2, 4, 0
+        };
+    }
+
+    public static byte getData(ChatColor color) {
+        if (color.ordinal() > 15) {
+            return 0;
+        }
+
+        return data[color.ordinal()];
+    }
+
+    public static ChatColor fromString(String source) {
+        return ChatColor.getByChar(source.replace("§", "").charAt(0));
+    }
+
+}
